2d8a93c517ef9edf98ac3c32d2aeb218b1568f50
Silvia <Silvia@SILVIA-NB>
2016-10-26 19:01:23 +0800

    加入專案檔案。

---
+++ MVC5HomeWork01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5HomeWork01.MvcApplication" Language="C#" %>
---
+++ MVC5HomeWork01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5HomeWork01.Startup))]
+namespace MVC5HomeWork01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
